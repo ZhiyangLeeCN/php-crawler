6a9eaeb54c088cc18811b07b34cd62cc1634e42a
zhiyanglee <zhiyanglee@foxmail.com>
2016-12-14 17:07:30 +0800

    示范demo更新以及添加.gitignore文件

--- demo/csdn_blog_crawler.php
+++ demo/csdn_blog_crawler.php
@@ -21,15 +21,15 @@ $httpDownloader = new HttpDownloader();
 //添加种子页面
 $scheduler->push(new Task("http://blog.csdn.net/"));
 
-//开始迭代链接并抓取
+//开始迭代链接进行抓取
 while (($task = $scheduler->next()) != null) {
 
-    //下载链接对应的内容并建立DOM对象进行解析
+    //获取任务指定的URL的响应内容
     $response = $httpDownloader->send('GET', $task->getUrl());
     $body = $response->getBody()->getContents();
     $htmlDom = new HtmlDom($body, $task->getUrl(), $task->getUrlHost());
 
-    //如果是首页只获取文章相关的链接并加入到调度器里面
+    //如果是首页，只获取文章详情页链接
     if (preg_match("/http:\\/\\/blog.csdn.net\\//", $task->getUrl())) {
 
         $htmlDom->filterXPath('//h3[@class="tracking-ad"]')->filter("a")->each(function(HtmlDom $node, $index) use($scheduler) {
@@ -40,7 +40,7 @@ while (($task = $scheduler->next()) != null) {
 
     }
 
-    //如果是文章详情页则打印文章标题
+    //如果是文章详情页，那么就打印每个文章详情页的标题
     if (preg_match("/http:\\/\\/blog.csdn.net\\/\w+\\/article\\/details\\/\d+/", $task->getUrl())) {
 
         $title = $htmlDom->filter('title')->html();
